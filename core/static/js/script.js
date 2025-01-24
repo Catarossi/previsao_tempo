a1c56e3e13a0c83f5df5ea6cf5b44d6a71171bd8
Catarossi <felipecatarossi.dev@gmail.com>
2025-01-24 09:16:58 -0300

    Adicionando preferencia por tema

--- core/static/js/script.js
+++ core/static/js/script.js
@@ -2,16 +2,40 @@ let body = document.body;
 let icon = document.getElementById("icon");
 let btn = document.getElementById("btn");
 
+// Função para alternar o tema
 function trade_theme() {
-    if (body.getAttribute('data-bs-theme') === 'dark') {
-        body.setAttribute('data-bs-theme', 'light');
-        icon.className = "fa-regular fa-moon";
-        icon.style.color = "#ffffff";
-        btn.style.backgroundColor = '#868B94';
+    const currentTheme = body.getAttribute('data-bs-theme'); // Obtém o tema atual
+    if (currentTheme === 'dark') {
+        setTheme('light');
     } else {
-        body.setAttribute('data-bs-theme', 'dark');
-        icon.className = "fa-regular fa-sun";
-        icon.style.color = "#868B94";
-        btn.style.backgroundColor = '#ffffff';
+        setTheme('dark');
     }
 }
+
+// Função para definir o tema e salvar no Local Storage
+function setTheme(theme) {
+    if (theme === 'dark') {
+        body.setAttribute('data-bs-theme', 'dark');
+        icon.className = "fa-regular fa-sun"; // Ícone para tema dark
+        icon.style.color = "#868B94"; // Cor do ícone
+        btn.style.backgroundColor = '#ffffff'; // Cor do botão
+    } else {
+        body.setAttribute('data-bs-theme', 'light');
+        icon.className = "fa-regular fa-moon"; // Ícone para tema light
+        icon.style.color = "#ffffff"; // Cor do ícone
+        btn.style.backgroundColor = '#868B94'; // Cor do botão
+    }
+
+    // Salva o tema no Local Storage
+    localStorage.setItem('theme', theme);
+}
+
+// Carrega o tema salvo no Local Storage ao carregar a página
+window.onload = () => {
+    const savedTheme = localStorage.getItem('theme'); // Recupera o tema salvo
+    if (savedTheme) {
+        setTheme(savedTheme); // Aplica o tema salvo
+    } else {
+        setTheme('light'); // Define o tema padrão como light
+    }
+};
