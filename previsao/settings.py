7724ba7df8a143bb0f1f7c8e642ac6a0c7657a8a
Catarossi <felipecatarossi.dev@gmail.com>
2025-03-07 10:12:49 -0300

    Deploy inicial

--- previsao/settings.py
+++ previsao/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)jgj4yk)$3mg8+#7ct-a#-u7lk2)=z49#==2i)jbk$*=)fyi5m'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['seu-projeto.onrender.com']
 
 
 # Application definition
@@ -75,11 +77,7 @@ WSGI_APPLICATION = 'previsao.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')}
 
 
 # Password validation
